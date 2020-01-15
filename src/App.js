84c6793eda6bdcfdc402ca7759f2fb7ac5d01f82
oleksandr.raspopov <alexander@ORASPOPOV-MAC.local>
2020-01-15 11:09:49 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+function useDataFetching(url) {
+  let [loading, setLoading] = useState(true);
+  let [result, setResult] = useState([]);
+  let [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let data = await fetch(url);
+        let json = await data.json();
+
+        if (json) {
+          setLoading(false);
+          setResult(json);
+        }
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
+      }
+      setLoading(false);
+    }
+
+    fetchData();
+  }, [url]);
+  return {
+    loading,
+    result,
+    error,
+  };
+}
+
+function ExchangeRate() {
+  let { loading, result, error } = useDataFetching(
+    "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11",
+  );
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+  return (
+    <ul>
+      {result.map(item => {
+        return (
+          <li key={item.ccy}>
+            <p>{item.ccy}</p>
+            <p>{item.buy}</p>
+            <p>{item.sale}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <ExchangeRate />;
 }
 
 export default App;
